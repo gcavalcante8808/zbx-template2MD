9def30ab178a5ec0aa27cdaa8103670c632b9efa
gcavalcante8808 <gabriel.cavalcante88@gmail.com>
2017-03-14 17:46:46 -0300

    Working Version.

--- template2MD.py
+++ template2MD.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import chardet
 import template_api
+
 from string import Template
-from enum import Enum
 
 
 template_info = Template("""## $visible_name\n
@@ -26,50 +27,53 @@ Description: $item_description
 """)
 
 ITEM_TYPES = [
-	"Zabbix agent",
-	"SNMPv1 agent",
-	"Zabbix trapper",
-	"Simple check",
-	"SNMPv2 agent",
-	"Zabbix internal",
-	"SNMPv3 agent",
-	"Zabbix agent (active)",
-	"Zabbix aggregate",
-	"Web item",
-	"external check",
-	"database monitor",
-	"IPMI agent",
-	"SSH agent",
-	"TELNET agent",
-	"calculated",
-	"JMX agent",
-	"SNMP trap"
+    "Zabbix agent",
+    "SNMPv1 agent",
+    "Zabbix trapper",
+    "Simple check",
+    "SNMPv2 agent",
+    "Zabbix internal",
+    "SNMPv3 agent",
+    "Zabbix agent (active)",
+    "Zabbix aggregate",
+    "Web item",
+    "external check",
+    "database monitor",
+    "IPMI agent",
+    "SSH agent",
+    "TELNET agent",
+    "calculated",
+    "JMX agent",
+    "SNMP trap"
 ]
 
-
-
-with open('zbx_export_templates.xml', 'r') as openfile:
-	zbx_template = openfile.read()
+with open('zbx_export_templates.xml', 'rb') as openfile:
+    zbx_template = openfile.read()
 
 content = template_api.parseString(zbx_template, silence=True)
 
 templates = content.templates.get_template()
 
 with open('template_doc.md', 'w') as openfile:
-	for template in templates:
-		openfile.write(template_info.substitute(template_name=template.template,
-			visible_name=template.name,
-			items_count=len(template.items.item),
-			template_description="Stub"
-			)
-		)
-		for item in template.items.item:
-			openfile.write(item_info.substitute(item_name=item.name.encode('utf-8'),
-				item_type=ITEM_TYPES[item.type_],
-				item_key=item.key,
-				item_delay=item.delay,
-				item_history=item.history,
-				item_trends=item.trends,
-				item_status=item.status,
-				item_description=item.description.encode('utf-8'))
-			)
+    for template in templates:
+
+        openfile.write(template_info.substitute(template_name=template.template,
+                                                visible_name=template.name.encode('utf-8'),
+                                                items_count=len(template.items.item),
+                                                template_description="Stub"
+                                                )
+                       )
+        for item in template.items.item:
+            try:
+                openfile.write(item_info.substitute(item_name=item.name.encode('utf-8'),
+                                                    item_type=ITEM_TYPES[item.type_],
+                                                    item_key=item.key,
+                                                    item_delay=item.delay,
+                                                    item_history=item.history,
+                                                    item_trends=item.trends,
+                                                    item_status=item.status,
+                                                    item_description=item.description.encode('utf-8'))
+                               )
+            except (UnicodeDecodeError,):
+                print('Object Skipped: %s->%s') %(template.name, item.name)
+                pass
